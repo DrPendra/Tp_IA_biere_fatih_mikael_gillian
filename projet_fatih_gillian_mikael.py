97549037e31a022a32e6b89c4234f3ae9ac12029
Gillian Catil <gigicatil@hotmail.fr>
2023-09-18 16:30:07 +0200

    mise à jour

--- projet_fatih_gillian_mikael.py
+++ projet_fatih_gillian_mikael.py
@@ -10,46 +10,8 @@ from sklearn.metrics import accuracy_score, classification_report
 
 pd.set_option('display.max_columns', None) # a check c'est quoi
 
-df = pd.read_csv('./recipeData.csv', encoding="latin1")
+df = pd.read_csv('./recipeData_clean.csv', encoding="latin1")
 
-orig_leng = len(df)
-
-df = df.drop(
- columns=["BeerID", "UserId", "URL", "Name", "Style", "PrimingMethod", "PrimingAmount", "PitchRate", "MashThickness",
-          "PrimaryTemp", "SugarScale"])  # PrimaryTemp
-df.dropna(inplace=True)
-ser = df.isna().mean() * 100
-
-# aze = df["Size(L)"].quantile(0.95)
-df = df[df["Size(L)"] <= df["Size(L)"].quantile(0.95)]
-df = df[df["OG"] <= df["OG"].quantile(0.95)]
-df = df[df["FG"] <= df["FG"].quantile(0.95)]
-df = df[(df["IBU"] <= 150) & (df["IBU"] > 0)]  #IBU max == 150 selon wikipedia & supérieur a zero
-df = df[df["BoilSize"] <= df["BoilSize"].quantile(0.95)]
-
-hist = df.hist(bins=50, log=True)
-
-new_len = len(df)
-# print(new_len / orig_leng)
-
-one_hot = pd.get_dummies(df["BrewMethod"])
-print(one_hot)
-df = df.drop(columns=["BrewMethod"])
-df = df.join(one_hot)
-
-print(df)
-
-
-plt.matshow(df.corr())
-plt.xticks(range(df.select_dtypes(['number']).shape[1]), df.select_dtypes(['number']).columns, fontsize=14, rotation=45)
-plt.yticks(range(df.select_dtypes(['number']).shape[1]), df.select_dtypes(['number']).columns, fontsize=14)
-cb = plt.colorbar()
-cb.ax.tick_params(labelsize=10)
-plt.title('Correlation Matrix', fontsize=16)
-
-#plt.show()
-plt.close()
-############### ^^"
 # Scatterplot pour mettre en avant => corrélation positive
 plt.figure()
 plt.scatter(df["ABV"], df["OG"])
@@ -75,7 +37,7 @@ y = df[["OG"]]
 
 #test corrélation avec IBU
 #2D
-
+'''
 plt.figure()
 plt.scatter(df["IBU"], df["OG"])
 plt.xlabel("IBU")
@@ -124,8 +86,8 @@ plt.ylabel("BoilSize")
 #plt.title("La corrélation linéaire de ABV et de OG")
 plt.show()
 plt.close()
-
-#
+'''
+# Regression Linear pour prédire AVB avec OG
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 RL = LinearRegression()
@@ -136,19 +98,19 @@ MSE = 1/len(y_test) * np.sum((y_predict - y_test)**2)
 print(MSE)
 MAE = 1/len(y_test) * np.sum(abs(y_predict - y_test))
 print(MAE)
-print(df[["FG"]])
 
-# X = pd.concat([one_hot, df[["feature1", "feature2"]]], axis=1)
-X = df[["StyleID", "Efficiency", "OG", "BoilSize", "ABV"]] # [[]]
+
+# Random Forest Classifier Linear pour prédire AVB avec OG
+X = df[["StyleID", "Efficiency", "OG", "BoilGravity", "ABV"]] # [[]]
 y = one_hot
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # modèle Random Forest
 rf_classifier = RandomForestClassifier(random_state=0)
-param_grid = {"criterion":["gini"], "max_depth":[20, 25, 30],"min_samples_leaf":[7, 10, 15],  "min_samples_split":[25, 30, 35], "max_features":[None]}
+param_grid = {"max_depth":[ 21,22, 23,24],"min_samples_leaf":[ 12, 13, 14],  "min_samples_split":[33, 35, 40], "max_features":[None]}
 
-search = HalvingGridSearchCV(rf_classifier, param_grid, resource='n_estimators', max_resources=110,random_state=0, n_jobs=-1, verbose=2).fit(X_train, y_train)
+search = HalvingGridSearchCV(rf_classifier, param_grid, resource='n_estimators', max_resources=150,random_state=0, n_jobs=-1, verbose=2).fit(X_train, y_train)
 print(search.best_params_, search.best_estimator_)
 rf_best =search.best_estimator_
 
